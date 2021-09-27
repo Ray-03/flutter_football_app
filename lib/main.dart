8c2824f46ddf1e04f8b6aeb9d6a9b5b709b7cac8
Ray-03 <rinaldy.richard.03@gmail.com>
2021-09-27 11:33:25 +0700

    set screen orientation to landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const FlutterFootballApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeLeft,
+    ],
+  );
+  runApp(const FlutterFootballApp());
+}
 
 class FlutterFootballApp extends StatelessWidget {
   const FlutterFootballApp({Key? key}) : super(key: key);
