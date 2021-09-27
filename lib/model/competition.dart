5adbe47c58daba92cc14c13439c1763b4bc3d28d
Ray-03 <rinaldy.richard.03@gmail.com>
2021-09-28 00:08:49 +0700

    delete unnecessary properties

--- lib/model/competition.dart
+++ lib/model/competition.dart
@@ -27,56 +27,24 @@ class Competition {
     int? id,
     Area? area,
     String? name,
-    String? code,
-    String? emblemUrl,
-    String? plan,
-    CurrentSeason? currentSeason,
-    int? numberOfAvailableSeasons,
-    String? lastUpdated,
   }) {
     _id = id;
     _area = area;
     _name = name;
-    _code = code;
-    _emblemUrl = emblemUrl;
-    _plan = plan;
-    _currentSeason = currentSeason;
-    _numberOfAvailableSeasons = numberOfAvailableSeasons;
-    _lastUpdated = lastUpdated;
   }
 
   Competition.fromJson(dynamic json) {
     _id = json['id'];
     _area = json['area'] != null ? Area.fromJson(json['area']) : null;
     _name = json['name'];
-    _code = json['code'];
-    _emblemUrl = json['emblemUrl'];
-    _plan = json['plan'];
-    _currentSeason = json['currentSeason'] != null
-        ? CurrentSeason.fromJson(json['currentSeason'])
-        : null;
-    _numberOfAvailableSeasons = json['numberOfAvailableSeasons'];
-    _lastUpdated = json['lastUpdated'];
   }
   int? _id;
   Area? _area;
   String? _name;
-  String? _code;
-  dynamic _emblemUrl;
-  String? _plan;
-  CurrentSeason? _currentSeason;
-  int? _numberOfAvailableSeasons;
-  String? _lastUpdated;
 
   int? get id => _id;
   Area? get area => _area;
   String? get name => _name;
-  String? get code => _code;
-  dynamic get emblemUrl => _emblemUrl;
-  String? get plan => _plan;
-  CurrentSeason? get currentSeason => _currentSeason;
-  int? get numberOfAvailableSeasons => _numberOfAvailableSeasons;
-  String? get lastUpdated => _lastUpdated;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
@@ -85,14 +53,6 @@ class Competition {
       map['area'] = _area?.toJson();
     }
     map['name'] = _name;
-    map['code'] = _code;
-    map['emblemUrl'] = _emblemUrl;
-    map['plan'] = _plan;
-    if (_currentSeason != null) {
-      map['currentSeason'] = _currentSeason?.toJson();
-    }
-    map['numberOfAvailableSeasons'] = _numberOfAvailableSeasons;
-    map['lastUpdated'] = _lastUpdated;
     return map;
   }
 }
