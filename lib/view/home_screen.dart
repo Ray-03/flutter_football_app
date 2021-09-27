ebad898350315ea7c1b4798632bd440ca0d362b5
Ray-03 <rinaldy.richard.03@gmail.com>
2021-09-27 15:59:38 +0700

    add grid view to home screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -7,7 +7,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Football App'),
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 250,
+          ),
+          itemBuilder: (BuildContext context, int index) => Card(
+            color: Colors.amber,
+            child: Center(
+              child: Text('$index'),
+            ),
+          ),
+          itemCount: 10,
+        ),
+      ),
     );
   }
 }
