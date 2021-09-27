f43f8db1d7498d639d8dde13eec969b1f699986e
Ray-03 <rinaldy.richard.03@gmail.com>
2021-09-28 00:01:12 +0700

    add fulltime model

---
+++ lib/model/fulltime.dart
@@ -0,0 +1,31 @@
+/// fullTime : {
+///     "homeTeam":0,
+///     "awayTeam":1
+/// }
+
+class FullTime {
+  FullTime({
+    int? homeTeam,
+    int? awayTeam,
+  }) {
+    _homeTeam = homeTeam;
+    _awayTeam = awayTeam;
+  }
+
+  FullTime.fromJson(dynamic json) {
+    _homeTeam = json['homeTeam'];
+    _awayTeam = json['awayTeam'];
+  }
+  int? _homeTeam;
+  int? _awayTeam;
+
+  int? get homeTeam => _homeTeam;
+  int? get awayTeam => _awayTeam;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['homeTeam'] = _homeTeam;
+    map['awayTeam'] = _awayTeam;
+    return map;
+  }
+}
