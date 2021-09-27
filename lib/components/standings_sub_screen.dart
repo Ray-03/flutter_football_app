91b235147a1acae66945a596898f89840defed27
Ray-03 <rinaldy.richard.03@gmail.com>
2021-09-27 20:23:42 +0700

    create table for standings

--- lib/components/standings_sub_screen.dart
+++ lib/components/standings_sub_screen.dart
@@ -5,6 +5,66 @@ class StandingsSubScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Table(
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: [
+            TableRow(
+              children: [
+                SizedBox(),
+                SizedBox(),
+                SizedBox(),
+                Text('Pt'),
+                Text('P'),
+                Text('W'),
+                Text('D'),
+                Text('L'),
+                Text('GF'),
+                Text('GA'),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text('1.'),
+                FlutterLogo(),
+                Text('Club Name'),
+                Text('Pt'),
+                Text('P'),
+                Text('W'),
+                Text('D'),
+                Text('L'),
+                Text('GF'),
+                Text('GA'),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text('2.'),
+                FlutterLogo(),
+                Text('Club Name'),
+                Text('Pt'),
+                Text('P'),
+                Text('W'),
+                Text('D'),
+                Text('L'),
+                Text('GF'),
+                Text('GA'),
+              ],
+            ),
+          ],
+          defaultColumnWidth: const IntrinsicColumnWidth(
+            flex: 0.15,
+          ),
+          border: TableBorder(
+            horizontalInside: BorderSide(width: 1),
+          ),
+          columnWidths: const {
+            2: FlexColumnWidth(),
+          },
+        ),
+      ),
+    );
   }
 }
