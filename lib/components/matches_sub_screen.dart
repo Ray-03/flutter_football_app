20b50e0d962d775c375629430c35b39701000839
Ray-03 <rinaldy.richard.03@gmail.com>
2021-09-28 08:24:00 +0700

    create matches template

--- lib/components/matches_sub_screen.dart
+++ lib/components/matches_sub_screen.dart
@@ -5,6 +5,37 @@ class MatchesSubScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text('date YYYY-MM-DD'),
+                GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 400,
+                    childAspectRatio: 16 / 9,
+                    mainAxisSpacing: 30,
+                    crossAxisSpacing: 30,
+                  ),
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      color: Colors.amber,
+                    );
+                  },
+                  itemCount: 3,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
