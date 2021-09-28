97cdf564d91c613efcd7de828791823066aedce8
Ray-03 <rinaldy.richard.03@gmail.com>
2021-09-28 13:10:01 +0700

    add style to available competition grid item

--- lib/components/available_competitions_grid.dart
+++ lib/components/available_competitions_grid.dart
@@ -18,51 +18,70 @@ class AvailableCompetitionsGrid extends StatelessWidget {
         AsyncSnapshot<List<Competition>> snapshot,
       ) {
         if (snapshot.hasData) {
-          return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              crossAxisSpacing: 30,
-              mainAxisSpacing: 30,
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              Competition item = snapshot.data!.elementAt(index);
-              return InkWell(
-                onTap: () => Navigator.pushNamed(
-                  context,
-                  CompetitionDetailsScreen.id,
-                  arguments: item,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    if (item.area!.ensignUrl != null)
-                      Expanded(
-                        child: SvgPicture.network(
-                          item.area!.ensignUrl!,
-                          semanticsLabel: 'Flag',
-                          fit: BoxFit.fitHeight,
-                          placeholderBuilder: (BuildContext context) =>
-                              const Center(
-                            child: CircularProgressIndicator(),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                crossAxisSpacing: 30,
+                mainAxisSpacing: 30,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                Competition item = snapshot.data!.elementAt(index);
+                return Card(
+                  margin: const EdgeInsets.only(top: 20),
+                  elevation: 4,
+                  shadowColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  // decoration: BoxDecoration(
+                  //   borderRadius: BorderRadius.circular(10),
+                  //   color: Colors.white70,
+                  // ),
+                  child: InkWell(
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      CompetitionDetailsScreen.id,
+                      arguments: item,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: item.area!.ensignUrl != null
+                                ? SvgPicture.network(
+                                    item.area!.ensignUrl!,
+                                    semanticsLabel: 'Flag',
+                                    fit: BoxFit.fitHeight,
+                                    placeholderBuilder:
+                                        (BuildContext context) => const Center(
+                                      child: CircularProgressIndicator(),
+                                    ),
+                                  )
+                                : Image.network(
+                                    'https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png',
+                                  ), // show image not found if ensignUrl null,
                           ),
                         ),
-                      )
-                    else
-                      Image.network(
-                        'https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png',
-                      ), // show image not found if ensignUrl null
-                    Text(item.name ?? '-'),
-                    Text(item.area!.name ?? '-'),
-                  ],
-                ),
-              );
-            },
-            itemCount: snapshot.data!.length,
+                        Text(item.name ?? '-'),
+                        Text(item.area!.name ?? '-'),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              itemCount: snapshot.data!.length,
+            ),
           );
         } else if (snapshot.hasError) {
           throw Error();
         } else {
-          return CircularProgressIndicator();
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         }
       },
     );
